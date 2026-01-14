adbd4b399d90c545be093af334ce0e0e3287cbc8
Baraa <156128855+Baraa-op@users.noreply.github.com>
2026-01-14 18:37:51 +0300

    Create 47FinishCode.cpp

---
+++ 47FinishCode.cpp
@@ -0,0 +1,60 @@
+//Advanced-47 2
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <ctime>
+#include <cstdlib>
+using namespace std  ;
+
+
+float Read_Number(string Message)
+{
+    float Number;
+    cout << Message;
+    cin >> Number ;
+
+    return Number ;
+}
+
+bool Is_Negative_Number(int Number)
+{
+    return Number < 0;
+}
+
+float GetFractionPart(float Number)
+{
+    return Number - int(Number) ;
+}
+
+
+
+int MyRound(float Number)
+{
+    int IntPart ;
+    IntPart = int(Number) ;
+    
+    float FractionPart  ;
+    FractionPart = GetFractionPart(Number) ;
+    if(abs(FractionPart) >= 0.5)
+    {
+        if(!Is_Negative_Number(Number))
+            return ++IntPart ;
+        else
+            return --IntPart;    
+    }
+    else
+    {
+        return IntPart ; 
+    }
+}
+
+
+int main()
+{
+    float Number = Read_Number("plese enter Number : " ) ;
+    cout << "\nmy round : " << MyRound(Number) ;
+    cout << "\nC++ round : " << round(Number);
+
+
+}
+
